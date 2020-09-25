77b0653b81c21978f3f720063c3ebef4f211d09c
Eirik Osnes <eirik.osnes@knowit.no>
2020-09-25 08:21:31 +0200

    Form generated from server data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Question from './Question';
-import {Questions, Answers} from '../types'
+import {Questions, Answers, FormDefinitionWithQuestions} from '../types'
 
 
 let questionFile: Questions = {};
 try { questionFile = require('../forms.json'); }
 catch (e) { console.warn("Cant find form.json") }
 
-export default function Form(props: {formDefinition: any}) {
+export default function Form(props: {formDefinition: FormDefinitionWithQuestions}) {
 
     function updateAnswer(key: string, rating: number): void {
         //Note asynchronicity, if really quick, rating might be unset.
@@ -16,21 +16,51 @@ export default function Form(props: {formDefinition: any}) {
         setAnswers(dummy);
     }
 
-    function createQuestions(): JSX.Element[] {
+    function createQuestions() {
+        if(!props.formDefinition) return [];
+
+        let formDef = props.formDefinition.data.getFormDefinition;
+
         let qs: JSX.Element[] = [];
-        Object.entries(questionFile).forEach(([key, value]) => {
-            qs.push(
-                <Question 
-                    key={key} 
-                    listID={key}
-                    topic={value.topic}
-                    text={value.text}
-                    updateAnswer={updateAnswer}
-                />
-            );
-        });
-        return qs;
+        console.log(props);
+        if(formDef.questions){
+            console.log("Hello");
+            if(formDef.questions.items)
+
+            for (let index = 0; index < formDef.questions.items.length; index++) {
+                const element = formDef.questions.items[index];
+                if (!element) continue;
+                qs.push(
+                    <Question 
+                        key={element.question.id} 
+                        listID={element.question.id}
+                        topic={element.question.topic}
+                        text={element.question.text}
+                        updateAnswer={updateAnswer}
+                    />
+                );
+            }
+        }
+        
+        console.log(qs);
+
+        // Object.entries(questionFile).forEach(([key, value]) => {
+        //     qs.push(
+        //         <Question 
+        //             key={key} 
+        //             listID={key}
+        //             topic={value.topic}
+        //             text={value.text}
+        //             updateAnswer={updateAnswer}
+        //         />
+        //     );
+        // });
+        setQuestions(qs);
     };
+
+    useEffect(() => {
+        createQuestions();
+    }, [props.formDefinition])
 
     function createAnswers(): Answers {
         let as = {} as Answers;
@@ -49,14 +79,13 @@ export default function Form(props: {formDefinition: any}) {
         console.log(answers);
     }
     
-    const [questions] = useState(createQuestions());
+    const [questions, setQuestions] = useState<JSX.Element[] | null>(null);
     const [answers, setAnswers] = useState(createAnswers());
 
     return (
         <div className="form">
-            {JSON.stringify(props.formDefinition)}
-            {console.log(props.formDefinition)} 
-            {/* {questions} */}
+            {questions}
+            {console.log(questions)}
             <button onClick={printAllAnswers}>Print all</button>
         </div>
     )
