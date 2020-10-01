f379914d961eb932c05fdafd2fc71f56697aaec4
fredstrin <fredrik.strindin@knowit.no>
2020-10-01 11:14:42 +0200

    added pagefile to store all  pages for routing

---
+++ src/components/pages/Pages.tsx
@@ -0,0 +1,6 @@
+
+export {default as AdminPage} from './AdminPage';
+export {default as AnswerPage} from './AnswerPage';
+export {default as HomePage} from './HomePage';
+export {default as StatPage} from './StatPage';
+export {default as UserPage} from './UserPage';
