e039736c1a3c387d3ea1dd088bb9557ca8fac599
fredstrin <fredrik.strindin@knowit.no>
2020-10-01 13:13:53 +0200

    moved most of functionality from app to content

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Form from './components/Form'
 import Amplify, {Auth, Hub, API, graphqlOperation} from 'aws-amplify';
 import awsconfig from './aws-exports';
 import * as mutations from './graphql/mutations';
-import * as queries from './graphql/custom-queries';
-import {AnsweredQuestion, Answers, UserFormCreated} from './types';
-import RadarPlot from './components/RadarPlot';
 import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
-import * as helper from './helperFunctions'
 import Content from './components/Content';
 
 Amplify.configure(awsconfig);
-
-const testData: AnsweredQuestion[] = [
-    {
-        question: {
-            text: "Text 1",
-            topic: "Topic 1",
-            group: "Group 1",
-            category: "Category 1"
-        },
-        answer: 2
-    }, {
-        question: {
-            text: "Text 2",
-            topic: "Topic 2",
-            group: "Group 1",
-            category: "Category 1"
-        },
-        answer: 3
-    }, {
-        question: {
-            text: "Text 3",
-            topic: "Topic 3",
-            group: "Group 1",
-            category: "Category 2"
-        },
-        answer: 3
-    }, {
-        question: {
-            text: "Text 4",
-            topic: "Topic 4",
-            group: "Group 1",
-            category: "Category 2"
-        },
-        answer: 5
-    }, {
-        question: {
-            text: "Text 5",
-            topic: "Topic 5",
-            group: "Group 1",
-            category: "Category 4"
-        },
-        answer: 2
-    },
-]
 
 let formDef = require('./form2.json')
 
 const App = () => {
     const [user, setUser] = useState<any | null>(null);
-    const [formDefinition, setFormDefinition] = useState<any | null>(null);
-    const [answers, setAnswers] = useState<Answers>();
-
-    const createAnswers = (): Answers => {
-        if(!formDefinition) return {};
-        let formDef = formDefinition.data.getFormDefinition;
-        let as = {} as Answers;
-        if(formDef.questions.items){
-            for (let index = 0; index < formDef.questions.items.length; index++) {
-                const element = formDef.questions.items[index];
-                if (!element) continue;
-                as[element.question.id] = {
-                    topic: element.question.topic,
-                    group: "knowledge",
-                    category: element.question.category,
-                    rating: null
-                }
-            }
-        }
-        return as;
-    };
-
-    const updateAnswer = (key: string, rating: number): void => {
-        let newAnswers = {...answers};
-        newAnswers[key].rating = rating;
-        setAnswers(newAnswers);
-    }
-
-    useEffect(() => {
-        setAnswers(createAnswers());
-    }, [formDefinition]);
-
-    //TODO: Need more refactoring
-    const createUserForm = async () => {
-        let userForm: UserFormCreated | undefined = (await helper.callGraphQL<UserFormCreated>(mutations.createUserForm, {input: {}})).data;
-        console.log(userForm);
-        if(!answers){
-            console.warn("answers is undefined");
-            return;
-        }
-        for (const [key, value] of Object.entries(answers)) {
-            if(!value.rating) continue;
-            API.graphql(graphqlOperation(
-                mutations.createQuestionAnswer, {input: {
-                    userFormID: userForm?.createUserForm.id, 
-                    answer: value.rating, 
-                    questionAnswerQuestionId: key
-                }
-            }))
-        }
-    }
+    
 
     useEffect(() => {
         Hub.listen('auth', ({ payload: { event, data } }) => {
@@ -130,16 +31,11 @@ const App = () => {
             }
         });
         getUser().then(userData => setUser(userData));
-        getFormDefinition().then(f => {
-            setFormDefinition(f)
-        });
     }, []);
 
-    async function getFormDefinition() {
-        return API.graphql(graphqlOperation(queries.getFormDefinitionWithQuestions, { id: "fd1" }));
-    }
+    
 
-    function getUser() {
+    const getUser = () => {
         return Auth.currentAuthenticatedUser()
             .then(userData => userData)
             .catch(() => console.log('Not signed in'));
@@ -169,17 +65,7 @@ const App = () => {
             <button onClick={() => sendFormDefinition()}>Send form definition to server</button>
             <AmplifySignOut />
             My App
-            {(!formDefinition) ? "" : 
-            <Content 
-                answerProps={{
-                    updateAnswer: updateAnswer,
-                    formDefinition: formDefinition,
-                    createUserForm: createUserForm
-                }}
-                statsProps= {{
-                    data: testData
-                }}
-            />}
+            <Content />
             {/*(!formDefinition) ? "" : <Form updateAnswer={updateAnswer} formDefinition={formDefinition} createUserForm={createUserForm} />*/}
         </div>
     );
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,13 +1,131 @@
-import React from 'react'
-import { FromAppProps } from '../types'
+import { API, graphqlOperation } from 'aws-amplify';
+import React, { useEffect, useState } from 'react'
+import { AnsweredQuestion, Answers, FromAppProps, UserFormCreated } from '../types'
 import Router from './Router'
+import * as helper from '../helperFunctions'
+import * as mutations from '../graphql/mutations';
+import * as queries from '../graphql/custom-queries';
+
+const testData: AnsweredQuestion[] = [
+    {
+        question: {
+            text: "Text 1",
+            topic: "Topic 1",
+            group: "Group 1",
+            category: "Category 1"
+        },
+        answer: 2
+    }, {
+        question: {
+            text: "Text 2",
+            topic: "Topic 2",
+            group: "Group 1",
+            category: "Category 1"
+        },
+        answer: 3
+    }, {
+        question: {
+            text: "Text 3",
+            topic: "Topic 3",
+            group: "Group 1",
+            category: "Category 2"
+        },
+        answer: 3
+    }, {
+        question: {
+            text: "Text 4",
+            topic: "Topic 4",
+            group: "Group 1",
+            category: "Category 2"
+        },
+        answer: 5
+    }, {
+        question: {
+            text: "Text 5",
+            topic: "Topic 5",
+            group: "Group 1",
+            category: "Category 4"
+        },
+        answer: 2
+    },
+]
 
 
-const Content = ({...props}: FromAppProps) => {
+const Content = () => {
+    
+    const [answers, setAnswers] = useState<Answers>();
+    const [formDefinition, setFormDefinition] = useState<any | null>(null);
+
+    const createAnswers = (): Answers => {
+        if(!formDefinition) return {};
+        let formDef = formDefinition.data.getFormDefinition;
+        let as = {} as Answers;
+        if(formDef.questions.items){
+            for (let index = 0; index < formDef.questions.items.length; index++) {
+                const element = formDef.questions.items[index];
+                if (!element) continue;
+                as[element.question.id] = {
+                    topic: element.question.topic,
+                    group: "knowledge",
+                    category: element.question.category,
+                    rating: null
+                }
+            }
+        }
+        return as;
+    };
+
+    const updateAnswer = (key: string, rating: number): void => {
+        let newAnswers = {...answers};
+        newAnswers[key].rating = rating;
+        setAnswers(newAnswers);
+    }
+
+    useEffect(() => {
+        setAnswers(createAnswers());
+    }, [formDefinition]);
+    
+    //TODO: Need more refactoring
+    const createUserForm = async () => {
+        let userForm: UserFormCreated | undefined = (await helper.callGraphQL<UserFormCreated>(mutations.createUserForm, {input: {}})).data;
+        console.log(userForm);
+        if(!answers){
+            console.warn("answers is undefined");
+            return;
+        }
+        for (const [key, value] of Object.entries(answers)) {
+            if(!value.rating) continue;
+            API.graphql(graphqlOperation(
+                mutations.createQuestionAnswer, {input: {
+                    userFormID: userForm?.createUserForm.id, 
+                    answer: value.rating, 
+                    questionAnswerQuestionId: key
+                }
+            }))
+        }
+    }
+
+    useEffect(() => {
+        getFormDefinition().then(f => {
+            setFormDefinition(f);
+        });
+    }, []);
+
+    async function getFormDefinition() {
+        return API.graphql(graphqlOperation(queries.getFormDefinitionWithQuestions, { id: "fd1" }));
+    }
 
     return(
         <div>
-            <Router {...props} />
+            <Router  
+                answerProps={{
+                    updateAnswer: updateAnswer,
+                    formDefinition: formDefinition,
+                    createUserForm: createUserForm
+                }}
+                statsProps={{
+                    data: testData
+                }} />
         </div>
     );
 
