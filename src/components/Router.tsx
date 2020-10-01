48c22049cc5125865c76880ea38f1f5e3a656cd6
fredstrin <fredrik.strindin@knowit.no>
2020-10-01 11:01:56 +0200

    added router

---
+++ src/components/Router.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+
+const Router = () => {
+
+
+
+
+
+    return(
+        <div>
+            Router
+        </div>
+    );
+
+};
+
+export default Router;
