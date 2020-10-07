4f25cb3760fa058668103c71856885f18c99e186
fredstrin <fredrik.strindin@knowit.no>
2020-10-07 09:48:36 +0200

    small renames and edits

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,29 @@
 import { AmplifySignOut } from '@aws-amplify/ui-react'
 import { AppBar, Button, Toolbar } from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
 import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
-      },
+    },
     navigation: {
-    flexGrow: 1,
+        flexGrow: 1,
     },
     logoutButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2),
     },
     button: {
-      width:"100px"
+        width: "100px"
     },
-  }));
+}));
 
 const NavBar = () => {
     const classes = useStyles();
     const history = useHistory();
 
-    function handleClick(path: string){
+    function handleClick(path: string) {
         history.push(path);
     }
 
@@ -34,11 +34,11 @@ const NavBar = () => {
                     <div className={classes.navigation}>
                         <Button variant="contained" onClick={() => handleClick("/home")} className={classes.button}>Home</Button>
                         <Button variant="contained" onClick={() => handleClick("/stats")} className={classes.button}>Statistics</Button>
-                        <Button variant="contained" onClick={() => handleClick("/answer")}  className={classes.button}>Answers</Button>
-                        <Button variant="contained" onClick={() => handleClick("/user")}  className={classes.button}>User</Button>
-                        <Button variant="contained" onClick={() => handleClick("/admin")}  className={classes.button}>Admin</Button>
+                        <Button variant="contained" onClick={() => handleClick("/answer")} className={classes.button}>Answers</Button>
+                        <Button variant="contained" onClick={() => handleClick("/user")} className={classes.button}>User</Button>
+                        <Button variant="contained" onClick={() => handleClick("/admin")} className={classes.button}>Admin</Button>
                     </div>
-                    <AmplifySignOut className={classes.logoutButton}/>
+                    <AmplifySignOut className={classes.logoutButton} />
                 </Toolbar>
             </AppBar>
         </div>
