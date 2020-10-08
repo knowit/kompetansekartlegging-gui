3498b8de5acc2a39ad8476556b12949792e77f03
Eirik Osnes <eirik.osnes@knowit.no>
2020-10-08 09:17:12 +0200

    cleaned helperFunctions.ts

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -5,24 +5,6 @@ export const callGraphQL = async <T>(query: any, variales?: {} | undefined): Pro
     return (await API.graphql(graphqlOperation(query, variales))) as GraphQLResult<T>;
 };
 
-export const callGraphQL2 = async <T>(query: any, variales?: {} | undefined): Promise<GraphQLResult<T>> => {
-    return (await API.graphql(graphqlOperation(query, variales))) as GraphQLResult<T>;
-};
-
-// export const getCallResult = <T>(query: any, variales?: {} | undefined): T | null => {
-//     let dat: T | null = null;
-//     callGraphQL<T>(query, variales).then(r => {
-//         if(typeof(r) === undefined) dat = null;
-//         else dat = r.data;
-//     });
-//     return dat;
-// };
-
-
-// getFormDefinition().then(f => {
-//     setFormDefinition(f);
-// });
-
 type SearchableItem = {
     createdAt:string
 }
@@ -32,3 +14,4 @@ export const getLastItem = <T extends SearchableItem>(itemsArray?:T[]) => {
     let sortedArray = itemsArray.sort((a,b) => (Date.parse(a.createdAt) > Date.parse(b.createdAt)) ? -1 : 1);
     return sortedArray[0];
 }
+
