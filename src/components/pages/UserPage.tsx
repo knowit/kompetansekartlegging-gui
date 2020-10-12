73202dd69358fb87b4d4660c9d083697cadda4a5
fredstrin <fredrik.strindin@knowit.no>
2020-10-12 15:04:01 +0200

    added safty delte delay

--- src/components/pages/UserPage.tsx
+++ src/components/pages/UserPage.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UserProps } from '../../types';
 
 
 
 const UserPage = ({...props}: UserProps) => {
 
+    const [deleteCounter, setDeleteCounter] = useState<number>(0);
+    const deleteMaxCounter = 3;
 
-
-
+    const deleteButtonPress = () => {
+        setDeleteCounter(deleteCounter + 1);
+        if(deleteCounter + 1 >= deleteMaxCounter){
+            setDeleteCounter(0);
+            props.deleteUserData();
+        }
+    };
 
     return(
         <div>
             <button onClick={props.listUserForms}>List my data</button>
-            <button onClick={props.deleteUserData}>Delete my data</button>
+            <button onClick={deleteButtonPress}>Delete my data</button>
+            {deleteCounter === 0 ? "" : (deleteCounter + "/" + deleteMaxCounter + " clicks untill data is deleted...")}
         </div>
     );
 
