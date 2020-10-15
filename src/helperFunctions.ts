06b19234ab9aa91b0ba26a9b6c106a47d6a26c17
MrMokka <sartaren@hotmail.com>
2020-10-15 13:09:59 +0200

    changed datatype in b from int to float

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -32,7 +32,7 @@ const splitArray = <T>(array: T[]): T[][] => {
 //For now: anytime using a backend environment, or lacking environment variables, the return must be set manually
 const getEnvTableID = () => {
     if(process.env.REACT_APP_ENV_TABLE_ID) return process.env.REACT_APP_ENV_TABLE_ID;
-    else return "c6oprrghzvemrnzcmy5hb73lqu-agnostic";
+    else return "ofjt5xeegrby3ekcwwyrzz2xv4-devdev";
 }
 
 export const callBatchGraphQL = async <T>(query: any, variables: {input: any[]}, table:string): Promise<GraphQLResult<T>[]> => {
