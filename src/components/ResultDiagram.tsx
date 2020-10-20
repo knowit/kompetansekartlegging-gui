514886632d9f3ae4aec28132a0d9783d7050fbd9
MrMokka <sartaren@hotmail.com>
2020-10-20 14:48:51 +0200

    new data save function, added react chartjs graph

--- src/components/ResultDiagram.tsx
+++ src/components/ResultDiagram.tsx
@@ -1,35 +1,31 @@
-import { ResponsiveRadar } from '@nivo/radar'
 import React, { useEffect, useState } from 'react'
+import { HorizontalBar } from 'react-chartjs-2';
 import { roundDecimals } from '../helperFunctions';
 import { AnswerData, CalculationData, ResultData } from '../types'
 
 export default function ResultDiagram(props: { data: AnswerData[] }) {
 
     const [answerData, setAnswerData] = useState<ResultData[]>([]);
-
-    let calcData: CalculationData[] = [];
-    let result: ResultData[] = [];
-
-    // useEffect(() => {
-    //     if(answerData.length === 0){
-    //         let newAnswerData: ResultData[] = [];
-    //         props.data.forEach(dat => {
-    //             let cat = newAnswerData.find(ans => ans.category === dat.category);
-    //             if(!cat){
-    //                 cat = {
-    //                     category: dat.category,
-    //                     knowledgeCount: 0,
-    //                     knowledgeTotal: 0,
-    //                     motivationCount: 0,
-    //                     motivationTotal: 0,
-    //                     questionIds: []
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }), [];
-
+    
     useEffect(() => {
+        if(answerData.length === 0){
+            let result: ResultData[] = [];
+            props.data.forEach(dat => {
+                let cat = result.find(res => res.category === dat.category);
+                if(!cat){
+                    result.push({
+                        category: dat.category,
+                        averageKnowledge: 0,
+                        averageMotivation: 0
+                    });
+                }
+            });
+            setAnswerData(result);
+        }
+    }, [props.data]);
+    
+    useEffect(() => {
+        let calcData: CalculationData[] = [];
         props.data.forEach(dat => {
             if(dat.knowledge < 0 && dat.motivation < 0) return;
             let catIndex = calcData.findIndex(calc => calc.category === dat.category);
@@ -53,58 +49,56 @@ export default function ResultDiagram(props: { data: AnswerData[] }) {
                 calcData[catIndex].motivationTotal += dat.motivation;
             }
         });
-        console.log(calcData);
         calcData.forEach(dat => {
-            let resIndex = result.findIndex(res => res.category === dat.category);
-            if(resIndex === -1){
-                resIndex = result.length;
-                result.push({
-                    category: dat.category,
-                    averageKnowledge: 0,
-                    averageMotivation: 0
-                });
-            }
-            result[resIndex].averageKnowledge = roundDecimals(dat.knowledgeTotal / dat.knowledgeCount || 0, 2);
-            result[resIndex].averageMotivation = roundDecimals(dat.motivationTotal / dat.motivationCount || 0, 2);
+            let answers = [...answerData];
+            let resIndex = answers.findIndex(ans => ans.category === dat.category);
+            if(resIndex === -1) return;
+            answers[resIndex].averageKnowledge = roundDecimals(dat.knowledgeTotal / dat.knowledgeCount || 0, 2);
+            answers[resIndex].averageMotivation = roundDecimals(dat.motivationTotal / dat.motivationCount || 0, 2);
+            setAnswerData(answers);
         });
-        // setAnswerData(result);
-        console.log(result);
     }, [props.data]);
 
     return (
-        <ResponsiveRadar
-            data={answerData}
-            keys={['averageKnowledge', 'averageMotivation']}
-            indexBy="category"
-            maxValue={5}
-            margin={{top: 70, right: 80, bottom: 40, left: 200}}
-            curve="linearClosed"
-            gridShape="linear"
-            dotSize={10}
-            enableDotLabel={true}
-            colors={{scheme: 'nivo'}}
-            blendMode="multiply"
-            legends={[
-                {
-                    anchor: 'top-left',
-                    direction: 'column',
-                    translateX: -50,
-                    translateY: -40,
-                    itemWidth: 80,
-                    itemHeight: 20,
-                    itemTextColor: '#999',
-                    symbolSize: 12,
-                    symbolShape: 'circle',
-                    effects: [
+        <HorizontalBar 
+            data={{
+                labels: answerData.map(value => value.category),
+                datasets: [
+                    {
+                        label: 'Knowledge',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: answerData.map(value => value.averageKnowledge)
+                    },
+                    {
+                        label: 'Motivation',
+                        backgroundColor: 'rgba(99,255,132,0.2)',
+                        borderColor: 'rgba(99,255,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: answerData.map(value => value.averageMotivation)
+                    }
+                ]
+            }}
+            height={300}
+            options={{
+                maintainAspectRatio: false,
+                scales: {
+                    xAxes: [
                         {
-                            on: 'hover',
-                            style: {
-                                itemTextColor: '#000'
+                            ticks: {
+                                beginAtZero: true,
+                                max: 5
                             }
                         }
                     ]
                 }
-            ]}
+            }}
+
         />
     );
 };
