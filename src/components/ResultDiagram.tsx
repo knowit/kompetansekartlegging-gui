fa97e38e7409ee5d23dc6b1e69f97c296580a7c5
MrMokka <sartaren@hotmail.com>
2020-10-23 09:36:53 +0200

    cleaned reasult diagram to design

--- src/components/ResultDiagram.tsx
+++ src/components/ResultDiagram.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { HorizontalBar } from 'react-chartjs-2';
 import { roundDecimals } from '../helperFunctions';
+import { KnowitColors } from '../styles';
 import { AnswerData, CalculationData, ResultData } from '../types'
 
 export default function ResultDiagram(props: { data: AnswerData[] }) {
@@ -66,8 +67,8 @@ export default function ResultDiagram(props: { data: AnswerData[] }) {
                 datasets: [
                     {
                         label: 'Knowledge',
-                        backgroundColor: 'red', //'rgba(255,99,132,0.2)',
-                        borderColor: 'red', //'rgba(255,99,132,1)',
+                        backgroundColor: KnowitColors.lightGreen, //'rgba(255,99,132,0.2)',
+                        //borderColor: 'red', //'rgba(255,99,132,1)',
                         borderWidth: 1,
                         // hoverBackgroundColor: 'rgba(255,99,132,0.4)',
                         // hoverBorderColor: 'rgba(255,99,132,1)',
@@ -75,8 +76,8 @@ export default function ResultDiagram(props: { data: AnswerData[] }) {
                     },
                     {
                         label: 'Motivation',
-                        backgroundColor: 'green',  //'rgba(99,255,132,0.2)',
-                        borderColor: 'green', //'rgba(99,255,132,1)',
+                        backgroundColor: KnowitColors.greyGreen,  //'rgba(99,255,132,0.2)',
+                        //borderColor: 'green', //'rgba(99,255,132,1)',
                         borderWidth: 1,
                         // hoverBackgroundColor: 'rgba(255,99,132,0.4)',
                         // hoverBorderColor: 'rgba(255,99,132,1)',
