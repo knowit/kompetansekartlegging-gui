c600eaaa9632ab7a763272c10792abb5492cb1c7
MrMokka <sartaren@hotmail.com>
2020-10-27 15:09:31 +0100

    trying out, not working well...

--- src/components/ResultDiagram.tsx
+++ src/components/ResultDiagram.tsx
@@ -4,8 +4,11 @@ import { roundDecimals } from '../helperFunctions';
 import { KnowitColors } from '../styles';
 import { AnswerData, CalculationData, ResultData } from '../types'
 import { makeStyles } from '@material-ui/core/styles'
+import { Icon } from '@material-ui/core';
+import { K0 } from '../icons/iconController';
+import { couldStartTrivia } from 'typescript';
 
-const style = makeStyles((theme) => ({
+const graphStyle = makeStyles((theme) => ({
     knowledgeChart: {
         width: '58.8%',
 
@@ -15,17 +18,70 @@ const style = makeStyles((theme) => ({
     }
 }));
 
+const plugins = [{
+    afterDraw: (chart: any) => {
+        console.log("After draw");
+        let ctx = chart.chart.ctx;
+        ctx.save();
+        let xAxis = chart.scales['x-axis-0'];
+        let yAxis = chart.scales['y-axis-0'];
+        xAxis.ticks.forEach((value: any, index: any) => {
+            let x = xAxis.getPixelForTick(index);
+            let image = new Image();
+            image.src = '../icons/K - ekspert.svg';
+            ctx.drawImage(image, x - 12, yAxis.bottom + 10);
+        });
+        ctx.restore();    
+    }
+}];
+
+const graphOptions = {
+    
+    plugins: {plugins},
+    maintainAspectRatio: false,
+    legend: {
+        labels: {
+            fontColor: 'black'
+        }
+    },
+    scales: {
+        yAxes: [{
+            gridLines: {
+                color: '#E4E0DC'
+            },
+            ticks: {
+                fontColor: 'black'
+            }
+        }],
+        xAxes: [{
+            gridLines: {
+                color: '#E4E0DC'
+            },
+            ticks: {
+                fontColor: 'black',
+                beginAtZero: true,
+                max: 5,
+                // callback: (/*value, index, values*/) => {
+                //     let img = new Image();
+                //     img.src = '../icons/K - ekspert.svg'
+                //     return img;
+                // }
+            }
+        }]
+    }
+};
+
 export default function ResultDiagram(props: { data: AnswerData[], boolDraw: boolean }) {
-    const classes = style();
+    const style = graphStyle();
 
     const [answerData, setAnswerData] = useState<ResultData[]>([]);
-    
+
     useEffect(() => {
-        if(answerData.length === 0){
+        if (answerData.length === 0) {
             let result: ResultData[] = [];
             props.data.forEach(dat => {
                 let cat = result.find(res => res.category === dat.category);
-                if(!cat){
+                if (!cat) {
                     result.push({
                         category: dat.category,
                         averageKnowledge: 0,
@@ -36,7 +92,7 @@ export default function ResultDiagram(props: { data: AnswerData[], boolDraw: boo
             setAnswerData(result);
         }
     }, [props.data]);
-    
+
     useEffect(() => {
         createData();
     }, [props.data, props.boolDraw]);
@@ -44,9 +100,9 @@ export default function ResultDiagram(props: { data: AnswerData[], boolDraw: boo
     const createData = () => {
         let calcData: CalculationData[] = [];
         props.data.forEach(dat => {
-            if(dat.knowledge < 0 && dat.motivation < 0) return;
+            if (dat.knowledge < 0 && dat.motivation < 0) return;
             let catIndex = calcData.findIndex(calc => calc.category === dat.category);
-            if(catIndex === -1){
+            if (catIndex === -1) {
                 catIndex = calcData.length;
                 calcData.push({
                     category: dat.category,
@@ -57,11 +113,11 @@ export default function ResultDiagram(props: { data: AnswerData[], boolDraw: boo
                     questionIds: []
                 });
             }
-            if(dat.knowledge >= 0){
+            if (dat.knowledge >= 0) {
                 calcData[catIndex].knowledgeCount += 1;
                 calcData[catIndex].knowledgeTotal += dat.knowledge;
             }
-            if(dat.motivation >= 0){
+            if (dat.motivation >= 0) {
                 calcData[catIndex].motivationCount += 1;
                 calcData[catIndex].motivationTotal += dat.motivation;
             }
@@ -69,116 +125,48 @@ export default function ResultDiagram(props: { data: AnswerData[], boolDraw: boo
         calcData.forEach(dat => {
             let answers = [...answerData];
             let resIndex = answers.findIndex(ans => ans.category === dat.category);
-            if(resIndex === -1) return;
+            if (resIndex === -1) return;
             answers[resIndex].averageKnowledge = roundDecimals(dat.knowledgeTotal / dat.knowledgeCount || 0, 1);
             answers[resIndex].averageMotivation = roundDecimals(dat.motivationTotal / dat.motivationCount || 0, 1);
             setAnswerData(answers);
         });
     };
 
-    // useEffect(() => {
-    //     console.log(props.boolDraw);
-    // }, [props.boolDraw]);
-
-    // TODO: del opp i to horizontalbar. drop labels i motivation. Splitt opp knowledge og motivation i to forskjellige
     return (
         <Fragment>
-            <div className={classes.knowledgeChart}>
-            <HorizontalBar 
-                redraw={props.boolDraw}
-                data={{
-                    labels: answerData.map(value => value.category),
-                    datasets: [
-                        {
-                            label: 'Knowledge',
-                            backgroundColor: KnowitColors.lightGreen, //'rgba(255,99,132,0.2)',
-                            //borderColor: 'red', //'rgba(255,99,132,1)',
-                            borderWidth: 1,
-                            // hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                            // hoverBorderColor: 'rgba(255,99,132,1)',
-                            data: answerData.map(value => value.averageKnowledge)
-                        }
-                    ]
-                }}
-                options={{
-                    maintainAspectRatio: false,
-                    legend: {
-                        labels: {
-                            fontColor: 'black'
-                        }
-                    },
-                    scales: {
-                        yAxes: [{
-                            gridLines: {
-                                color: '#E4E0DC'
-                            },
-                            ticks: {
-                                fontColor: 'black'
+            <div className={style.knowledgeChart}>
+                <HorizontalBar
+                    redraw={props.boolDraw}
+                    data={{
+                        labels: answerData.map(value => value.category),
+                        datasets: [
+                            {
+                                label: 'Knowledge',
+                                backgroundColor: KnowitColors.lightGreen,
+                                borderWidth: 1,
+                                data: answerData.map(value => value.averageKnowledge)
                             }
-                        }],
-                        xAxes: [{
-                            gridLines: {
-                                color: '#E4E0DC'
-                            },
-                            ticks: {
-                                fontColor: 'black',
-                                beginAtZero: true,
-                                max: 5
-                            }
-                        }]
-                    }
-                }}
-    
-            />
+                        ]
+                    }}
+                    options={graphOptions}
+                />
             </div>
-            <div className={classes.motivationChart}>
-
-            <HorizontalBar 
-                redraw={props.boolDraw}
-                data={{
-                    labels: answerData.map(value => " "),
-                    datasets: [
-                        {
-                            label: 'Motivation',
-                            backgroundColor: KnowitColors.greyGreen,  //'rgba(99,255,132,0.2)',
-                            //borderColor: 'green', //'rgba(99,255,132,1)',
-                            borderWidth: 1,
-                            // hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                            // hoverBorderColor: 'rgba(255,99,132,1)',
-                            data: answerData.map(value => value.averageMotivation)
-                        }
-                    ]
-                }}
-                options={{
-                    maintainAspectRatio: false,
-                    // legend: {
-                    //     labels: {
-                    //         fontColor: 'black'
-                    //     }
-                    // },
-                    scales: {
-                        yAxes: [{
-                            gridLines: {
-                                color: '#E4E0DC'
-                            },
-                            ticks: {
-                                fontColor: 'black'
+            <div className={style.motivationChart}>
+                <HorizontalBar
+                    redraw={props.boolDraw}
+                    data={{
+                        labels: answerData.map(value => " "),
+                        datasets: [
+                            {
+                                label: 'Motivation',
+                                backgroundColor: KnowitColors.greyGreen,
+                                borderWidth: 1,
+                                data: answerData.map(value => value.averageMotivation)
                             }
-                        }],
-                        xAxes: [{
-                            gridLines: {
-                                color: '#E4E0DC'
-                            },
-                            ticks: {
-                                fontColor: 'black',
-                                beginAtZero: true,
-                                max: 5
-                            }
-                        }]
-                    }
-                }}
-    
-            />
+                        ]
+                    }}
+                    options={graphOptions}
+                />
             </div>
         </Fragment>
     );
