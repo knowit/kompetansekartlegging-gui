a31702e5e70ac14d85f647e9ecc584e0611f8948
larsbrek <73285117+larsbrek@users.noreply.github.com>
2020-10-28 12:34:06 +0100

    Added dropdown menu with sign out

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
-import { AppBar, Button, Toolbar, Avatar } from '@material-ui/core'
+import { AppBar, Button, Toolbar, Avatar, Menu, MenuItem, ClickAwayListener, Popper, Grow, Paper, MenuList } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { Auth } from 'aws-amplify';
 import React, { useEffect, useState } from 'react'
 import { useHistory } from "react-router-dom";
 import { KnowitColors } from '../styles';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -43,6 +44,10 @@ const NavBar = (user : any) => {
     const history = useHistory();
     const [userName, setUserName] = useState<string>('');
     const [userPicture, setUserPicture] = useState<string>('');
+    // const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [open, setOpen] = useState(false);
+    const anchorRef = React.useRef<HTMLButtonElement>(null);
+
 
     useEffect(() => {
         if (typeof user != "undefined" && user.user.hasOwnProperty("attributes")) {
@@ -52,14 +57,81 @@ const NavBar = (user : any) => {
         } 
     }, [user]);
 
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleCloseSignout = (event: React.MouseEvent<EventTarget>) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+          return;
+        }
+        
+        setOpen(false);
+        Auth.signOut();
+    };
+
+    const handleClose = (event: React.MouseEvent<EventTarget>) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+          return;
+        }
+        
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event: React.KeyboardEvent) {
+        if (event.key === 'Tab') {
+          event.preventDefault();
+          setOpen(false);
+        }
+      }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+        anchorRef.current!.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar className={classes.header}>
                     <div className={classes.userName}>{userName}</div>
-                    <Avatar className={classes.userPicture} src={userPicture} />
-                    <Button variant="contained" className={classes.logoutButton} onClick={() => Auth.signOut()}>Sign out</Button> 
+                    
+                    
+                    {/* <Button variant="contained" className={classes.logoutButton} onClick={() => Auth.signOut()}>Sign out</Button>  */}
+                    <div>
+                    <Button
+                        ref={anchorRef}
+                        aria-controls={open ? 'menu-list-grow' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleToggle}
+                    >
+                        <Avatar className={classes.userPicture} src={userPicture}
+                     />
+                    </Button>
+                <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                {({ TransitionProps, placement }) => (
+                    <Grow
+                    {...TransitionProps}
+                    style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                    >
+                    <Paper>
+                        <ClickAwayListener onClickAway={handleClose}>
+                        <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                            <MenuItem onClick={handleCloseSignout}>Sign out</MenuItem>
+                        </MenuList>
+                        </ClickAwayListener>
+                    </Paper>
+                    </Grow>
+                )}
+                </Popper>
+                    </div>
                 </Toolbar>
             </AppBar>
         </div>
