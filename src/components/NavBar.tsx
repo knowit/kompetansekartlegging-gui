6ecf90f4abede319dec808d6436c1c4f4241a8bc
Thor <thor.ellefsen@knowit.no>
2020-11-04 10:05:11 +0100

    Added menu items with confirmation dialog

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { AppBar, Button, Toolbar, Avatar, Menu, MenuItem, ClickAwayListener, Popper, Grow, Paper, MenuList } from '@material-ui/core'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { Auth } from 'aws-amplify';
 import React, { useEffect, useState } from 'react'
@@ -46,6 +47,7 @@ const NavBar = (user : any) => {
     const [userName, setUserName] = useState<string>('');
     const [userPicture, setUserPicture] = useState<string>('');
     const [avatarMenuOpen, setAvatarMenuOpen] = useState<boolean>(false);
+    const [deleteAlertOpen, setDeleteAlertOpen] = useState<boolean>(false);
 
     const anchorRef = React.useRef<HTMLButtonElement>(null);
 
@@ -86,6 +88,24 @@ const NavBar = (user : any) => {
         }
       }
 
+    const handleDeleteAnswers = (event: React.MouseEvent<EventTarget>) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+          return;
+        }
+        setDeleteAlertOpen(true);
+    };
+
+    const handleDisplayAnswers = (event: React.MouseEvent<EventTarget>) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+          return;
+        }
+        setAvatarMenuOpen(false);
+    };
+
+    const handleCloseAlert = () => {
+        setDeleteAlertOpen(false);
+      };
+
     // return focus to the button when we transitioned from !avatarMenuOpen -> avatarMenuOpen
     const avatarMenuPrevOpen = React.useRef(avatarMenuOpen);
 
@@ -125,6 +145,8 @@ const NavBar = (user : any) => {
                     <Paper>
                         <ClickAwayListener onClickAway={handleClose}>
                         <MenuList autoFocusItem={avatarMenuOpen} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                            <MenuItem onClick={handleDisplayAnswers}>Vis alle lagrede svar</MenuItem>
+                            <MenuItem onClick={handleDeleteAnswers}>Slett alle svar</MenuItem>
                             <MenuItem onClick={handleCloseSignout}>Logg ut</MenuItem>
                         </MenuList>
                         </ClickAwayListener>
@@ -132,6 +154,29 @@ const NavBar = (user : any) => {
                     </Grow>
                 )}
                 </Popper>
+                <Dialog
+                    open={deleteAlertOpen}
+                    onClose={handleCloseAlert}
+                    aria-labelledby="dialogtitle"
+                    aria-describedby="dialogdescription"
+                >
+                    <DialogTitle id="dialogtitle">
+                        {"Ønsker du å slette svarene dine?"}
+                    </DialogTitle>
+                    <DialogContent>
+                    <DialogContentText id="dialogdescription">
+                        OBS: Dette vil slette alle innsendte og lagrede svar!
+                    </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleCloseAlert} color="primary">
+                        Bekreft
+                    </Button>
+                    <Button onClick={handleCloseAlert} color="primary" autoFocus>
+                        Avbryt
+                    </Button>
+                    </DialogActions>
+                </Dialog>
                     </div>
                 </Toolbar>
             </AppBar>
