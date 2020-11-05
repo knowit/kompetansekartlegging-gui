d2b7da9698908e7b4b65e2aa1560e302a4a1f9c1
fredstrin <fredrik.strindin@knowit.no>
2020-11-05 15:47:10 +0100

    small fix and cleanups

--- src/components/AnswerDiagram.tsx
+++ src/components/AnswerDiagram.tsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { HorizontalBar } from 'react-chartjs-2';
-import { limitStringLength } from '../helperFunctions';
+import { addLeftPaddingToStringArray, limitStringLength } from '../helperFunctions';
 import { KnowitColors } from '../styles';
 import { AnswerData} from '../types'
 import { makeStyles } from '@material-ui/core/styles'
@@ -10,9 +10,11 @@ import clsx from 'clsx'
 
 const graphStyle = makeStyles({
     chartK: {
-        width: '55%',
+        width: '55vw',
         maxHeight: '80%',
-        marginRight: 20
+        marginRight: 20,
+        display: 'flex',
+        flexDirection: 'column',
     },
     chartM: {
         width: '45%',
@@ -26,7 +28,8 @@ const graphStyle = makeStyles({
         justifyContent: 'space-between',
     },
     iconBarK: {
-        marginLeft: '18%'
+        width: '84%',
+        alignSelf: 'flex-end'
     },
     icon: {
         height: '100%'
@@ -64,7 +67,7 @@ const graphOptions = {
                 fontColor: 'black',
                 callback: (value: string) => {
                     if(value === " ") return " ";
-                    return limitStringLength(value, 25, false)//.padStart(30, " ");
+                    return addLeftPaddingToStringArray(limitStringLength(value, 25, true), 30, " ");
                 }
             }
         }],
@@ -88,64 +91,6 @@ const graphOptions = {
 
 export default function AnswerDiagram(props: { data: AnswerData[], activeCategory: string }) {
     const style = graphStyle();
-
-    // const [answerData, setAnswerData] = useState<AnswerGraphResultData[]>([]);
-
-    // useEffect(() => {
-    //     if (answerData.length === 0) {
-    //         let result: AnswerGraphResultData[] = [];
-    //         props.data.forEach(dat => {
-    //             let cat = result.find(res => res.category === dat.category);
-    //             if (!cat) {
-    //                 result.push({
-    //                     category: dat.category,
-    //                     averageKnowledge: 0,
-    //                     averageMotivation: 0
-    //                 });
-    //             }
-    //         });
-    //         setAnswerData(result);
-    //     }
-    // }, [props.data]);
-
-    // useEffect(() => {
-    //     createData();
-    // }, [props.data]);
-
-    // const createData = () => {
-    //     let calcData: CalculationData[] = [];
-    //     props.data.forEach(dat => {
-    //         if (dat.knowledge < 0 && dat.motivation < 0) return;
-    //         let catIndex = calcData.findIndex(calc => calc.category === dat.category);
-    //         if (catIndex === -1) {
-    //             catIndex = calcData.length;
-    //             calcData.push({
-    //                 category: dat.category,
-    //                 knowledgeCount: 0,
-    //                 knowledgeTotal: 0,
-    //                 motivationCount: 0,
-    //                 motivationTotal: 0,
-    //                 questionIds: []
-    //             });
-    //         }
-    //         if (dat.knowledge >= 0) {
-    //             calcData[catIndex].knowledgeCount += 1;
-    //             calcData[catIndex].knowledgeTotal += dat.knowledge;
-    //         }
-    //         if (dat.motivation >= 0) {
-    //             calcData[catIndex].motivationCount += 1;
-    //             calcData[catIndex].motivationTotal += dat.motivation;
-    //         }
-    //     });
-    //     calcData.forEach(dat => {
-    //         let answers = [...answerData];
-    //         let resIndex = answers.findIndex(ans => ans.category === dat.category);
-    //         if (resIndex === -1) return;
-    //         answers[resIndex].averageKnowledge = roundDecimals(dat.knowledgeTotal / dat.knowledgeCount || 0, 1);
-    //         answers[resIndex].averageMotivation = roundDecimals(dat.motivationTotal / dat.motivationCount || 0, 1);
-    //         setAnswerData(answers);
-    //     });
-    // };
 
     return (
         <div className={style.container}>
