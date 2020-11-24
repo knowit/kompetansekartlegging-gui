ce83daceb9f468221d66b285e9b3df2b421c8a63
larsbrek <lars.brekka@knowit.no>
2020-11-24 11:30:55 +0100

    styling in overview moved to component

--- src/components/cards/Overview.tsx
+++ src/components/cards/Overview.tsx
@@ -1,17 +1,84 @@
-import { useScrollTrigger } from '@material-ui/core';
+import { makeStyles, Theme, useScrollTrigger } from '@material-ui/core';
 import clsx from 'clsx';
 import React, { Fragment, useEffect, useState } from 'react'
-import { CardStyle, OverviewStyle } from '../../styles';
+import { KnowitColors } from '../../styles';
 import { OverviewProps } from '../../types';
 import RadarPlot from '../RadarPlot';
 import ResultDiagram from '../ResultDiagram';
 import Highlights from '../Highlights';
 import CloseIcon from '@material-ui/icons/Close';
 
+const cardCornerRadius: number = 40;
+const zIndex: number = 50;
+
+const OverviewStyle = makeStyles({
+    root: {
+        maxHeight: '40%',
+        width: "100%",
+        backgroundColor: KnowitColors.white
+    },
+    radarPlot: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        overflowY: 'auto',
+        justifyContent: 'center'
+    },
+    cardHeader: {
+        display: "flex",
+        marginTop: cardCornerRadius,
+        height: cardCornerRadius
+    },
+    closeButton: {
+        marginTop: "3px",
+        marginRight: "32px",
+        '&:hover': {
+            color: KnowitColors.darkGreen
+        }
+    },
+    empty: {
+        display: "none"
+    },
+
+    // card
+    cardButton: {
+        fontWeight: "bold",
+        fontSize: 18,
+        padding: 10,
+        border: "none",
+        outline: "none",
+        backgroundColor: "transparent",
+        textAlign: "left",
+        paddingLeft: 50,
+        width: "100%"
+    },
+    closed: {
+        position: 'relative',
+        marginTop: -cardCornerRadius,
+        boxShadow: "0px 3px 2px grey",
+        borderBottomLeftRadius: cardCornerRadius,
+        borderBottomRightRadius: cardCornerRadius,
+        zIndex: zIndex
+    },
+    open: {
+        position: 'relative',
+        marginTop: -cardCornerRadius,
+        display: 'flex',
+        flexDirection: 'column',
+        overflowY: 'auto',
+        height: '100%',
+        boxShadow: "0px 3px 2px grey",
+        borderBottomLeftRadius: cardCornerRadius,
+        borderBottomRightRadius: cardCornerRadius,
+        zIndex: zIndex
+    },
+});
+
+  
+
 
 export const Overview = ({...props}: OverviewProps) => {
     const style = OverviewStyle();
-    const cardStyle = CardStyle({zIndex: 50});
 
     const buttonClick = () => {
         //TODO: Find a way to replace hadcode int with a something like enum (enum dont work)
@@ -20,11 +87,11 @@ export const Overview = ({...props}: OverviewProps) => {
 
     
     return (
-        <div className={clsx(style.root, props.commonCardProps.active ? cardStyle.open : cardStyle.closed)}>
+        <div className={clsx(style.root, props.commonCardProps.active ? style.open : style.closed)}>
             <div className={style.cardHeader}>
                 <button 
                     onClick={buttonClick} 
-                    className={clsx(cardStyle.cardButton)}
+                    className={clsx(style.cardButton)}
                 >
                     OVERSIKT
                 </button>
