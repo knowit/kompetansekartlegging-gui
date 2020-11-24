8447a962f9d93fcdcd6c9c0970a54ab9eb41af5f
larsbrek <lars.brekka@knowit.no>
2020-11-24 14:40:34 +0100

    moved style to scaledescription component

--- src/components/cards/ScaleDescription.tsx
+++ src/components/cards/ScaleDescription.tsx
@@ -1,35 +1,90 @@
 import clsx from 'clsx';
 import React from 'react';
-import { CardStyle, ScaleDescStyle } from '../../styles';
+import { KnowitColors, ScaleDescStyle } from '../../styles';
 import { ScaleDescriptionProps } from '../../types';
 import CloseIcon from '@material-ui/icons/Close';
 import DescriptionTable from '../DescriptionTable';
+import { makeStyles } from '@material-ui/core';
+
+const cardCornerRadius: number = 40;
+const zIndex: number = 40;
+
+const useStyles = makeStyles({
+    root: {
+        maxHeight: '32%',
+        width: "100%",
+        backgroundColor: KnowitColors.ecaluptus
+    },
+    cardHeader: {
+        display: "flex",
+        marginTop: cardCornerRadius,
+        height: cardCornerRadius
+    },
+    closeButton: {
+        marginTop: "3px",
+        marginRight: "32px",
+        '&:hover': {
+            color: KnowitColors.darkGreen
+        }
+    },
+
+    // card
+    cardButton: {
+        fontWeight: "bold",
+        fontSize: 18,
+        padding: 10,
+        border: "none",
+        outline: "none",
+        backgroundColor: "transparent",
+        textAlign: "left",
+        paddingLeft: 50,
+        width: "100%"
+    },
+    closed: {
+        position: 'relative',
+        marginTop: -cardCornerRadius,
+        boxShadow: "0px 3px 2px grey",
+        borderBottomLeftRadius: cardCornerRadius,
+        borderBottomRightRadius: cardCornerRadius,
+        zIndex: zIndex
+    },
+    open: {
+        position: 'relative',
+        marginTop: -cardCornerRadius,
+        display: 'flex',
+        flexDirection: 'column',
+        overflowY: 'auto',
+        height: '100%',
+        boxShadow: "0px 3px 2px grey",
+        borderBottomLeftRadius: cardCornerRadius,
+        borderBottomRightRadius: cardCornerRadius,
+        zIndex: zIndex
+    },
+})
 
 
 export const ScaleDescription = ({...props}: ScaleDescriptionProps) => {
-    const style = ScaleDescStyle();
-    const cardStyle = CardStyle({zIndex: 40});
+    const classes = useStyles();
 
     const buttonClick = () => {
         //TODO: Find a way to replace harcoded int with a something like enum (enum dont work)
         props.commonCardProps.setActiveCard(props.commonCardProps.index,  !props.commonCardProps.active);
     };
 
-    
 
     return (
-        <div className={clsx(style.root, props.commonCardProps.active ? cardStyle.open : cardStyle.closed)}>
-            <div className={style.cardHeader}>
+        <div className={clsx(classes.root, props.commonCardProps.active ? classes.open : classes.closed)}>
+            <div className={classes.cardHeader}>
                 <button 
                     onClick={buttonClick} 
-                    className={clsx(cardStyle.cardButton)}
+                    className={clsx(classes.cardButton)}
                 >
                     SKALABESKRIVELSE 
                 </button>
                 {props.commonCardProps.active ? (
                         <CloseIcon 
                             fontSize="large" 
-                            className={style.closeButton}
+                            className={classes.closeButton}
                             onClick={buttonClick}    
                         />
                     ) : null}
