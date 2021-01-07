4d7d49811cde70140354523c10c0ac58a54840c9
larsbrek <lars.brekka@knowit.no>
2021-01-07 10:06:27 +0100

    descriptiontable refactor

--- src/components/DescriptionTableMobile.tsx
+++ src/components/DescriptionTableMobile.tsx
@@ -38,12 +38,6 @@ const DescTableStyle = makeStyles({
         flexDirection: 'column',
         padding: 5
     },
-    top: {
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: 10,
-        paddingBottom: 20
-    },
     bottom: {
         display: 'flex'
     },
@@ -69,19 +63,17 @@ const DescTableStyle = makeStyles({
     }
 });
 
-// type headingAndText = {
-//     [
-//         heading : string,
-//         text : string
-//     ]
-// }
+
+type ContainerObject = {
+    icon: JSX.Element,
+    heading: string,
+    text: string
+}
 
 export const DescriptionTableMobile = () => {
     const style = DescTableStyle();
 
-    // const competenceTexts = [<Icon.K0/>, <Icon.K1/>, <Icon.K2/>, <Icon.K3/>, <Icon.K4/>, <Icon.K5/>]
-    // const competenceTexts = [<Icon.K0 className={style.iconArea} />, <Icon.K1 className={style.iconArea} />, <Icon.K2 className={style.iconArea} />, <Icon.K3 className={style.iconArea} />, <Icon.K4 className={style.iconArea} />, <Icon.K5 className={style.iconArea} />]
-    const COMPETENCE = [
+    const COMPETENCE : ContainerObject[] = [
         {icon: <Icon.K0 className={style.iconArea} />, heading: 'Kjenner ikke til området', text: ''}, 
         {icon: <Icon.K1 className={style.iconArea} />, heading: 'Noe innsikt', text: '(Har noe innsikt i området, samt evne til å resonnere over eller løse oppgaver på et ikke-profesjonelt nivå innenfor området)'}, 
         {icon: <Icon.K2 className={style.iconArea} />, heading: 'Potensielt brukbar kompetanse', text: '(Kompetanse som enten ikke er testet i oppdrag eller der man inntil videre trenger støtte fra andre i teamet)'}, 
@@ -89,8 +81,8 @@ export const DescriptionTableMobile = () => {
         {icon: <Icon.K4 className={style.iconArea} />, heading: 'Superstjerne', text: '(Har særdeles god kontroll og en etablert posisjon på området)'}, 
         {icon: <Icon.K5 className={style.iconArea} />, heading: 'Ekspert', text: '(En etterspurt spesialist, som fungerer som nyskapende eller strategisk kraft på området)'}, 
     ]
-    // const motivationIcons = [<Icon.M0 className={style.iconArea} />, <Icon.M1 className={style.iconArea} />, <Icon.M2 className={style.iconArea} />, <Icon.M3 className={style.iconArea} />, <Icon.M4 className={style.iconArea} />, <Icon.M5 className={style.iconArea} />]
-    const MOTIVATION = [
+
+    const MOTIVATION : ContainerObject[] = [
         {icon: <Icon.M0 className={style.iconArea} />, heading: 'Nei. Ønsker ikke å jobbe med dette', text: ''}, 
         {icon: <Icon.M1 className={style.iconArea} />, heading: 'Egentlig ikke. Ønsker svært lite å jobbe med', text: ''}, 
         {icon: <Icon.M2 className={style.iconArea} />, heading: 'Nja. Kan motvillig jobbe med dette', text: ''}, 
@@ -99,13 +91,13 @@ export const DescriptionTableMobile = () => {
         {icon: <Icon.M5 className={style.iconArea} />, heading: 'Perfekt. Dette er akkurat det jeg ønsker meg', text: ''}, 
     ]
 
-    const Container = (icon : JSX.Element[], heading : string[], text : string[]) => (
+    const Container = ({...props} : ContainerObject) => (
         <div className={style.container}>
             <div className={style.bottom}>
-                <div className={style.iconArea}>{icon}</div>
+                <div className={style.iconArea}>{props.icon}</div>
                 <div className={style.textBlock}>
-                    <div className={style.heading}>{heading}</div>
-                    <div className={style.text}>{text}</div>
+                    <div className={style.heading}>{props.heading}</div>
+                    <div className={style.text}>{props.text}</div>
                 </div>
             </div>
         </div>
@@ -119,69 +111,16 @@ export const DescriptionTableMobile = () => {
                     <div>KOMPETANSESKALA</div>
                 </div>
                 {
-                    COMPETENCE.map((icon, heading, text) => {
-                        <Container icon={icon} heading={heading} text={text}/>
-                    })
+                    COMPETENCE.map((obj) => (<Container icon={obj.icon} heading={obj.heading} text={obj.text}/>))
                 }
             </div>
             <div className={style.scaleRow}>
                 <div className={style.scaleTitle}>
                     <div>MOTIVASJONSSKALA</div>
                 </div>
-                <div className={style.container}>
-                    <div className={style.bottom}>
-                        <div className={style.iconArea}><Icon.M0 className={style.iconArea} /></div>
-                        <div className={style.textBlock}>
-                            <div className={style.heading}>Nei. Ønsker ikke å jobbe med dette</div>
-                            <div className={style.text}></div>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.container}>
-                    <div className={style.bottom}>
-                    <div className={style.iconArea}><Icon.M1 className={style.iconArea} /></div>
-                        <div className={style.textBlock}>
-                            <div className={style.heading}>Egentlig ikke. Ønsker svært lite å jobbe med</div>
-                            <div className={style.text}></div>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.container}>
-                    <div className={style.bottom}>
-                    <div className={style.iconArea}><Icon.M2 className={style.iconArea} /></div>
-                        <div className={style.textBlock}>
-                            <div className={style.heading}>Nja. Kan motvillig jobbe med dette</div>
-                            <div className={style.text}></div>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.container}>
-                    <div className={style.bottom}>
-                    <div className={style.iconArea}><Icon.M3 className={style.iconArea} /></div>
-                        <div className={style.textBlock}>
-                            <div className={style.heading}>OK. Ikke det jeg liker best, men helt i orden hvis jeg må</div>
-                            <div className={style.text}></div>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.container}>
-                    <div className={style.bottom}>
-                    <div className={style.iconArea}><Icon.M4 className={style.iconArea} /></div>
-                        <div className={style.textBlock}>
-                            <div className={style.heading}>Godt. Kjempefint å få jobbe med dette</div>
-                            <div className={style.text}></div>
-                        </div>
-                    </div>
-                </div>
-                <div className={style.container}>
-                    <div className={style.bottom}>
-                    <div className={style.iconArea}><Icon.M5 className={style.iconArea} /></div>
-                        <div className={style.textBlock}>
-                            <div className={style.heading}>Perfekt. Dette er akkurat det jeg ønsker meg</div>
-                            <div className={style.text}></div>
-                        </div>
-                    </div>
-                </div>
+                {
+                    MOTIVATION.map((obj) => (<Container icon={obj.icon} heading={obj.heading} text={obj.text}/>))
+                }
             </div>
         </div>
     );
