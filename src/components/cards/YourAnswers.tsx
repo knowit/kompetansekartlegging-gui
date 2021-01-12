4c668cabacb0e6a4605f51aa440c5818cf49baa4
larsbrek <lars.brekka@knowit.no>
2021-01-12 16:35:06 +0100

    refactor

--- src/components/cards/YourAnswers.tsx
+++ src/components/cards/YourAnswers.tsx
@@ -1,26 +1,7 @@
-import { Button, makeStyles } from '@material-ui/core';
-import clsx from 'clsx';
 import React, { useState } from 'react';
-import { KnowitColors } from '../../styles';
 import { YourAnswerProps } from '../../types';
 import { YourAnswersMobile } from '../YourAnswersMobile';
 import { YourAnswersDesktop } from '../YourAnswersDesktop';
-import { Panel } from '../Content';
 
 
-export const YourAnswers = ({ ...props }: YourAnswerProps) => {
-    return (
-        props.isMobile ? 
-            <YourAnswersMobile
-                {...props}
-                setIsCategorySubmitted={props.setIsCategorySubmitted}
-                alerts={props.alerts}
-            />
-        :
-            <YourAnswersDesktop 
-                {...props}
-                setIsCategorySubmitted={props.setIsCategorySubmitted}
-                alerts={props.alerts}
-            />
-    );
-};
+export const YourAnswers = ({ ...props }: YourAnswerProps) => props.isMobile ? <YourAnswersMobile {...props} /> : <YourAnswersDesktop {...props} />
