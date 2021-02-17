be8981613f8714c15a1ea0aa98e5d432c2d4a1e0
Wahed Mangal <mangal.wahed@gmail.com>
2021-02-17 14:51:11 +0100

    feat(desktop/scale-description): added dark backdrop that closes on click

--- src/components/FloatingScaleDescButton.tsx
+++ src/components/FloatingScaleDescButton.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
-import { Fab, Tooltip, makeStyles } from "@material-ui/core";
+import { Fab, Tooltip, Modal, makeStyles } from "@material-ui/core";
 import { KnowitColors } from "../styles";
 import DescriptionTable from "./DescriptionTable";
 
@@ -20,6 +20,7 @@ const floatingScaleDescButtonStyleDesktop = makeStyles({
         fontSize: "11px",
         lineHeight: "13px",
         height: "35px",
+        zIndex: 1301, // modal backdrop z-index + 1
     },
     fabMenu: {
         position: "absolute",
@@ -32,9 +33,12 @@ const floatingScaleDescButtonStyleDesktop = makeStyles({
         backgroundColor: KnowitColors.lightGreen,
         width: "400px",
         // viewport height - headerbar height - bottom margin height - other spacing
-	maxHeight: "calc(100vh - 66px - 55px - 20px)",
+        maxHeight: "calc(100vh - 66px - 55px - 20px)",
         boxShadow:
             "0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)",
+        "&:focus": {
+            outline: "none",
+        },
     },
     closeButton: {
         marginTop: "15px",
@@ -66,7 +70,7 @@ const floatingScaleDescButtonStyleDesktop = makeStyles({
             left: "50%",
             boxShadow: "0px 0px 5px 0px rgba(0, 0, 0, 0.50)",
         },
-    }
+    },
 });
 
 const floatingScaleDescButtonStyleMobile = makeStyles({
@@ -112,18 +116,19 @@ const floatingScaleDescButtonStyleMobile = makeStyles({
 
 type FloatingScaleDescButtonProps = {
     isMobile: boolean;
-    scaleDescOpen: boolean,
-    setScaleDescOpen: Dispatch<SetStateAction<boolean>>,
-    firstTimeLogin: boolean,
+    scaleDescOpen: boolean;
+    setScaleDescOpen: Dispatch<SetStateAction<boolean>>;
+    firstTimeLogin: boolean;
 };
 
 type ConditionalWrapProps = {
-    condition: boolean,
-    wrap: (children: JSX.Element) => JSX.Element,
-    children: JSX.Element,
-}
+    condition: boolean;
+    wrap: (children: JSX.Element) => JSX.Element;
+    children: JSX.Element;
+};
 
-const ConditionalWrap = ({condition, wrap, children} : ConditionalWrapProps) => condition ? wrap(children) : children;
+const ConditionalWrap = ({ condition, wrap, children }: ConditionalWrapProps) =>
+    condition ? wrap(children) : children;
 
 const FloatingScaleDescButton = ({
     isMobile,
@@ -135,49 +140,59 @@ const FloatingScaleDescButton = ({
         ? floatingScaleDescButtonStyleMobile()
         : floatingScaleDescButtonStyleDesktop();
 
-    const [showTooltip, setShowTooltip] = useState(firstTimeLogin)
+    const [showTooltip, setShowTooltip] = useState(firstTimeLogin);
     useEffect(() => {
         if (firstTimeLogin) {
             setTimeout(() => setShowTooltip(false), 5000);
         }
-    }, [firstTimeLogin])
+    }, [firstTimeLogin]);
 
     const handleMobileFabClick = () => {
         setShowTooltip(false);
         setScaleDescOpen((scaleDescOpen) => !scaleDescOpen);
-    }
+    };
 
     return (
         <>
             {scaleDescOpen && (
-                <div className={style.fabMenu}>
+                <Modal
+                    open={scaleDescOpen}
+                    onClose={() =>
+                        setScaleDescOpen((scaleDescOpen) => !scaleDescOpen)
+                    }
+                >
+                    <div className={style.fabMenu}>
                         <DescriptionTable
                             onClose={() => setScaleDescOpen(false)}
                             isMobile={isMobile}
                         />
-                    <div className={style.arrow}></div>
-                </div>
+                        <div className={style.arrow}></div>
+                    </div>
+                </Modal>
             )}
-            {isMobile ?
-             <ConditionalWrap condition={firstTimeLogin} wrap={children =>
-                 <Tooltip
-                     title="Trykk her for å se hva ikonene betyr!"
-                     open={showTooltip}
-                     arrow
-                 >
-                     {children}
-                 </Tooltip>
-             }>
-                 <Fab
-                     size="medium"
-                     variant="round"
-                     className={style.fab}
-                     onClick={handleMobileFabClick}
-                 >
-                     ?
-                 </Fab>
-             </ConditionalWrap>
-            :
+            {isMobile ? (
+                <ConditionalWrap
+                    condition={firstTimeLogin}
+                    wrap={(children) => (
+                        <Tooltip
+                            title="Trykk her for å se hva ikonene betyr!"
+                            open={showTooltip}
+                            arrow
+                        >
+                            {children}
+                        </Tooltip>
+                    )}
+                >
+                    <Fab
+                        size="medium"
+                        variant="round"
+                        className={style.fab}
+                        onClick={handleMobileFabClick}
+                    >
+                        ?
+                    </Fab>
+                </ConditionalWrap>
+            ) : (
                 <Fab
                     variant="extended"
                     className={style.fab}
@@ -187,7 +202,7 @@ const FloatingScaleDescButton = ({
                 >
                     SKALABESKRIVELSE
                 </Fab>
-            }
+            )}
         </>
     );
 };
