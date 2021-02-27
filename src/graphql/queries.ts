ce33016cb6615ecdf1ef7537dcf69c116a0bf37b
Wahed Mangal <mangal.wahed@gmail.com>
2021-02-27 15:01:14 +0100

    fix: updated amplify and fixed auth

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -3,321 +3,313 @@
 // this is an auto generated file. This will be overwritten
 
 export const getFormDefinition = /* GraphQL */ `
-    query GetFormDefinition($id: ID!) {
-        getFormDefinition(id: $id) {
-            id
-            createdAt
-            sortKeyConstant
-            questions {
-                nextToken
-            }
-            updatedAt
-        }
+  query GetFormDefinition($id: ID!) {
+    getFormDefinition(id: $id) {
+      id
+      createdAt
+      sortKeyConstant
+      questions {
+        nextToken
+      }
+      updatedAt
     }
+  }
 `;
 export const listFormDefinitions = /* GraphQL */ `
-    query ListFormDefinitions(
-        $filter: ModelFormDefinitionFilterInput
-        $limit: Int
-        $nextToken: String
-    ) {
-        listFormDefinitions(
-            filter: $filter
-            limit: $limit
-            nextToken: $nextToken
-        ) {
-            items {
-                id
-                createdAt
-                sortKeyConstant
-                updatedAt
-            }
-            nextToken
-        }
+  query ListFormDefinitions(
+    $filter: ModelFormDefinitionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listFormDefinitions(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        createdAt
+        sortKeyConstant
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
 export const getUserForm = /* GraphQL */ `
-    query GetUserForm($id: ID!) {
-        getUserForm(id: $id) {
-            id
-            createdAt
-            owner
-            formDefinitionID
-            questionAnswers {
-                nextToken
-            }
-            formDefinition {
-                id
-                createdAt
-                sortKeyConstant
-                updatedAt
-            }
-            updatedAt
-        }
+  query GetUserForm($id: ID!) {
+    getUserForm(id: $id) {
+      id
+      createdAt
+      owner
+      formDefinitionID
+      questionAnswers {
+        nextToken
+      }
+      formDefinition {
+        id
+        createdAt
+        sortKeyConstant
+        updatedAt
+      }
+      updatedAt
     }
+  }
 `;
 export const listUserForms = /* GraphQL */ `
-    query ListUserForms(
-        $filter: ModelUserFormFilterInput
-        $limit: Int
-        $nextToken: String
-    ) {
-        listUserForms(filter: $filter, limit: $limit, nextToken: $nextToken) {
-            items {
-                id
-                createdAt
-                owner
-                formDefinitionID
-                updatedAt
-            }
-            nextToken
-        }
+  query ListUserForms(
+    $filter: ModelUserFormFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUserForms(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        createdAt
+        owner
+        formDefinitionID
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
 export const getQuestionAnswer = /* GraphQL */ `
-    query GetQuestionAnswer($id: ID!) {
-        getQuestionAnswer(id: $id) {
-            id
-            userFormID
-            questionID
-            knowledge
-            motivation
-            question {
-                id
-                text
-                topic
-                qid
-                index
-                formDefinitionID
-                categoryID
-                createdAt
-                updatedAt
-            }
-            createdAt
-            updatedAt
-            owner
-        }
+  query GetQuestionAnswer($id: ID!) {
+    getQuestionAnswer(id: $id) {
+      id
+      userFormID
+      questionID
+      knowledge
+      motivation
+      question {
+        id
+        text
+        topic
+        qid
+        index
+        formDefinitionID
+        categoryID
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+      owner
     }
+  }
 `;
 export const listQuestionAnswers = /* GraphQL */ `
-    query ListQuestionAnswers(
-        $filter: ModelQuestionAnswerFilterInput
-        $limit: Int
-        $nextToken: String
-    ) {
-        listQuestionAnswers(
-            filter: $filter
-            limit: $limit
-            nextToken: $nextToken
-        ) {
-            items {
-                id
-                userFormID
-                questionID
-                knowledge
-                motivation
-                createdAt
-                updatedAt
-                owner
-            }
-            nextToken
-        }
+  query ListQuestionAnswers(
+    $filter: ModelQuestionAnswerFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listQuestionAnswers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        userFormID
+        questionID
+        knowledge
+        motivation
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
     }
+  }
 `;
 export const getQuestion = /* GraphQL */ `
-    query GetQuestion($id: ID!) {
-        getQuestion(id: $id) {
-            id
-            text
-            topic
-            qid
-            index
-            formDefinitionID
-            categoryID
-            category {
-                id
-                text
-                description
-                index
-                createdAt
-                updatedAt
-            }
-            createdAt
-            updatedAt
-        }
+  query GetQuestion($id: ID!) {
+    getQuestion(id: $id) {
+      id
+      text
+      topic
+      qid
+      index
+      formDefinitionID
+      categoryID
+      category {
+        id
+        text
+        description
+        index
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
     }
+  }
 `;
 export const listQuestions = /* GraphQL */ `
-    query ListQuestions(
-        $filter: ModelQuestionFilterInput
-        $limit: Int
-        $nextToken: String
-    ) {
-        listQuestions(filter: $filter, limit: $limit, nextToken: $nextToken) {
-            items {
-                id
-                text
-                topic
-                qid
-                index
-                formDefinitionID
-                categoryID
-                createdAt
-                updatedAt
-            }
-            nextToken
-        }
+  query ListQuestions(
+    $filter: ModelQuestionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listQuestions(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        text
+        topic
+        qid
+        index
+        formDefinitionID
+        categoryID
+        createdAt
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
 export const getCategory = /* GraphQL */ `
-    query GetCategory($id: ID!) {
-        getCategory(id: $id) {
-            id
-            text
-            description
-            index
-            createdAt
-            updatedAt
-        }
+  query GetCategory($id: ID!) {
+    getCategory(id: $id) {
+      id
+      text
+      description
+      index
+      createdAt
+      updatedAt
     }
+  }
 `;
 export const listCategorys = /* GraphQL */ `
-    query ListCategorys(
-        $filter: ModelCategoryFilterInput
-        $limit: Int
-        $nextToken: String
-    ) {
-        listCategorys(filter: $filter, limit: $limit, nextToken: $nextToken) {
-            items {
-                id
-                text
-                description
-                index
-                createdAt
-                updatedAt
-            }
-            nextToken
-        }
+  query ListCategorys(
+    $filter: ModelCategoryFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCategorys(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        text
+        description
+        index
+        createdAt
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
 export const getGroup = /* GraphQL */ `
-    query GetGroup($id: ID!) {
-        getGroup(id: $id) {
-            id
-            groupLeaderID
-            groupLeader {
-                id
-                groupID
-                createdAt
-                updatedAt
-            }
-            createdAt
-            updatedAt
-        }
+  query GetGroup($id: ID!) {
+    getGroup(id: $id) {
+      id
+      groupLeaderID
+      groupLeader {
+        id
+        groupID
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
     }
+  }
 `;
 export const listGroups = /* GraphQL */ `
-    query ListGroups(
-        $filter: ModelGroupFilterInput
-        $limit: Int
-        $nextToken: String
-    ) {
-        listGroups(filter: $filter, limit: $limit, nextToken: $nextToken) {
-            items {
-                id
-                groupLeaderID
-                createdAt
-                updatedAt
-            }
-            nextToken
-        }
+  query ListGroups(
+    $filter: ModelGroupFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listGroups(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        groupLeaderID
+        createdAt
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
 export const getUser = /* GraphQL */ `
-    query GetUser($id: ID!) {
-        getUser(id: $id) {
-            id
-            groupID
-            group {
-                id
-                groupLeaderID
-                createdAt
-                updatedAt
-            }
-            createdAt
-            updatedAt
-        }
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
+      id
+      groupID
+      group {
+        id
+        groupLeaderID
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
     }
+  }
 `;
 export const listUsers = /* GraphQL */ `
-    query ListUsers(
-        $filter: ModelUserFilterInput
-        $limit: Int
-        $nextToken: String
-    ) {
-        listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
-            items {
-                id
-                groupID
-                createdAt
-                updatedAt
-            }
-            nextToken
-        }
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        groupID
+        createdAt
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
 export const formByCreatedAt = /* GraphQL */ `
-    query FormByCreatedAt(
-        $sortKeyConstant: String
-        $createdAt: ModelStringKeyConditionInput
-        $sortDirection: ModelSortDirection
-        $filter: ModelFormDefinitionFilterInput
-        $limit: Int
-        $nextToken: String
+  query FormByCreatedAt(
+    $sortKeyConstant: String
+    $createdAt: ModelStringKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelFormDefinitionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    formByCreatedAt(
+      sortKeyConstant: $sortKeyConstant
+      createdAt: $createdAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
     ) {
-        formByCreatedAt(
-            sortKeyConstant: $sortKeyConstant
-            createdAt: $createdAt
-            sortDirection: $sortDirection
-            filter: $filter
-            limit: $limit
-            nextToken: $nextToken
-        ) {
-            items {
-                id
-                createdAt
-                sortKeyConstant
-                updatedAt
-            }
-            nextToken
-        }
+      items {
+        id
+        createdAt
+        sortKeyConstant
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
 export const userFormByCreatedAt = /* GraphQL */ `
-    query UserFormByCreatedAt(
-        $owner: String
-        $createdAt: ModelStringKeyConditionInput
-        $sortDirection: ModelSortDirection
-        $filter: ModelUserFormFilterInput
-        $limit: Int
-        $nextToken: String
+  query UserFormByCreatedAt(
+    $owner: String
+    $createdAt: ModelStringKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelUserFormFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    userFormByCreatedAt(
+      owner: $owner
+      createdAt: $createdAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
     ) {
-        userFormByCreatedAt(
-            owner: $owner
-            createdAt: $createdAt
-            sortDirection: $sortDirection
-            filter: $filter
-            limit: $limit
-            nextToken: $nextToken
-        ) {
-            items {
-                id
-                createdAt
-                owner
-                formDefinitionID
-                updatedAt
-            }
-            nextToken
-        }
+      items {
+        id
+        createdAt
+        owner
+        formDefinitionID
+        updatedAt
+      }
+      nextToken
     }
+  }
 `;
