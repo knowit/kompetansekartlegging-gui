d00b60992d84353a18ce56b3eaa85410b933173f
hakoncarlsen <hakon.carlsen@knowit.no>
2021-12-20 15:02:51 +0100

    personalisert import.py, og andre autogenererte forandringer.

--- scripts/import.py
+++ scripts/import.py
@@ -2,13 +2,13 @@ import boto3
 import os
 
 sourceappid = '3hic5nngffevtfafcd62sdoece'
-destinationappid = 'yyddd4muhbehvd33aad462lsom'
+destinationappid = 'njh3ylrpkfa3xkn4zd57gubixq'
 env = 'dev'
 
 tablenames = ["User", "UserForm", "Category", "FormDefinition", "Group", "Question", "QuestionAnswer"]
 
-clientsession = boto3.Session(profile_name='KompetanseDynamoReadOnly')
-targetclientsession = boto3.Session(profile_name='EUCentralAmplify')
+clientsession = boto3.Session(profile_name='kompetanse_dynamodb')
+targetclientsession = boto3.Session(profile_name='knowit_sandbox')
 
 dynamoclient = clientsession.client('dynamodb')
 
@@ -16,7 +16,7 @@ dynamotargetclient = targetclientsession.client('dynamodb')
 for tablename in tablenames:
     dynamopaginator = dynamoclient.get_paginator('scan')
     tabname=tablename + '-' + sourceappid + '-dev'
-    targettabname=tablename + '-' + destinationappid + '-sandbox'
+    targettabname=tablename + '-' + destinationappid + '-hakondev'
     dynamoresponse = dynamopaginator.paginate(
         TableName=tabname,
         Select='ALL_ATTRIBUTES',
