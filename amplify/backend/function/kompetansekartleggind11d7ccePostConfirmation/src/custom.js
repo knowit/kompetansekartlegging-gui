099b47b880fc0b71b5cf999b7410c3d70fc2ee84
hakoncarlsen <hakon.carlsen@knowit.no>
2022-01-10 19:59:38 +0100

    added initial functionality for adding user to cognito group in post-confirmation

--- amplify/backend/function/kompetansekartleggind11d7ccePostConfirmation/src/custom.js
+++ amplify/backend/function/kompetansekartleggind11d7ccePostConfirmation/src/custom.js
@@ -1,10 +1,47 @@
-var AWS = require('aws-sdk');
+const { CognitoIdentityProviderClient, AdminAddUserToGroupCommand } = require("@aws-sdk/client-cognito-identity-provider");
 
-exports.handler = (event, context, callback) => {
+const getGroupName = event => {
 
 
-  console.log('eyyy!');
+  const providerToGroup = {
+    "Google": "orgobjectnet",
+    "azureprovider": "orgother"
+  };
 
-  callback(null, event);
+  const identities = JSON.parse(event["request"]["userAttributes"]["identities"]);
+  const providerName = identities[0]["providerName"];
+
+  const groupName = providerToGroup[providerName];
+  return groupName;
 };
- 
+
+
+exports.handler = async (event, context, callback) => {
+
+
+  const client = new CognitoIdentityProviderClient({
+    region: event["region"]
+  });
+
+
+  groupName = getGroupName(event);
+
+  const input = {
+    UserPoolId: event["userPoolId"],
+    Username: event["userName"],
+    GroupName: groupName
+  }
+
+  const command = new AdminAddUserToGroupCommand(input);
+
+
+  try{
+    const response = await client.send(command);
+    console.log('response', response);
+  }catch(err){
+    console.log('error',err);
+  }finally{
+    callback(null, event);
+  }
+
+};  
