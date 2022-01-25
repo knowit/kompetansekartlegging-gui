8d963c741f09dc600dc2843e1c7ea4a778a75143
Kristian Stamland <kristian.stamland@knowit.no>
2022-01-25 10:32:49 +0100

    Changed migrate_cognito_users script

--- scripts/migrate_cognito_users.py
+++ scripts/migrate_cognito_users.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import csv
 
 with open('migrate_parameters.json') as parameters_file:
     parameters = json.load(parameters_file)
@@ -30,25 +31,43 @@ nextToken = None
 response = source_cognito_client.list_users(
     UserPoolId=source_userpool_id,
 )
+# file = open("importer.csv", "x")
+# csvFile = csv.DictWriter(f=file, fieldnames=["name","given_name","family_name","middle_name","nickname","preferred_username","profile","picture","website","email","email_verified","gender","birthdate","zoneinfo","locale","phone_number","phone_number_verified","address","updated_at","custom:OrganizationID","cognito:mfa_enabled","cognito:username"])
+
+# csvFile.writeheader()
+
 
 for user in response["Users"]:
-    user["Attributes"].append({"Name":"custom:OrganizationID", "Value": "knowitobjectnet"})
+    attributes = []
+    csvUser = {}
+    for attribute in user["Attributes"]:
+        if attribute["Name"] == "sub" or attribute["Name"] == "identities":
+            continue
+        attributes.append(attribute)
+    attributes.append({"Name":"custom:OrganizationID", "Value": "knowitobjectnet"})
+#     for attribute in attributes:
+#         csvUser[attribute["Name"]] = attribute["Value"]
+#     csvUser["cognito:username"] = user["Username"]
+#     csvFile.writerow(csvUser)
+
+# file.close()
+    user["Attributes"] = attributes
     destination_cognito_client.admin_create_user(
         UserPoolId=destination_userpool_id,
         Username=user["Username"],
         UserAttributes=user["Attributes"]
     )
 
-if response["PaginationToken"]:
-    nextToken = response["PaginationToken"]
+# if response["PaginationToken"]:
+#     nextToken = response["PaginationToken"]
 
-while (nextToken):
-    response = source_cognito_client.list_users(
-        UserpoolId=source_userpool_id,
-        PaginationToken=nextToken
-    )
-    if response["PaginationToken"]:
-        nextToken = response["PaginationToken"]
-    else:
-        nextToken = None
-        break
+# while (nextToken):
+#     response = source_cognito_client.list_users(
+#         UserpoolId=source_userpool_id,
+#         PaginationToken=nextToken
+#     )
+#     if response["PaginationToken"]:
+#         nextToken = response["PaginationToken"]
+#     else:
+#         nextToken = None
+#         break
