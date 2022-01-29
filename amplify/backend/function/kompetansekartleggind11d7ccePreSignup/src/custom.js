f51660f30019a3f1edf3e008122afefe9b5dc1b9
Kristian Stamland <kristian.stamland@knowit.no>
2022-01-29 20:23:35 +0100

    Made it so that presignup creates a cognito user if none exists, and links the provider to that new account

--- amplify/backend/function/kompetansekartleggind11d7ccePreSignup/src/custom.js
+++ amplify/backend/function/kompetansekartleggind11d7ccePreSignup/src/custom.js
@@ -44,7 +44,33 @@ exports.handler = async (event, context, callback) => {
       const [ providerName, providerUserId ] = event.userName.split('_') // event userName example: "Facebook_12324325436"
       await linkProviderToUser(userRs.Users[0].Username, event.userPoolId, providerName, providerUserId)
     } else {
-      console.log('user not found, skip.')
+      attributes = [];
+      Object.keys(event.request.userAttributes).forEach(key => {
+        if (key === "identities" || key === "sub" || key.includes("cognito:")) return;
+        attributes.push({"Name": key, "Value": event.request.userAttributes[key]});
+      })
+      if (event.userName != event.request.userAttributes.email){
+        console.log("Creating new Cognito User Pool user", attributes)
+        console.log(await new Promise((resolve, reject) => {
+          cognitoIdp.adminCreateUser({
+            MessageAction: "SUPPRESS",
+            Username: event.request.userAttributes.email,
+            UserAttributes: attributes,
+            UserPoolId: event.userPoolId
+          }, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          })
+        })
+        );
+        const [ providerName, providerUserId ] = event.userName.split('_');
+        await linkProviderToUser(event.request.userAttributes.email, event.userPoolId, providerName, providerUserId);
+      } else {
+        console.log('user not found, added new cognito user to userPool.');
+      }
     }
  
   }
--- scripts/migrate_users.py
+++ scripts/migrate_users.py
@@ -12,91 +12,90 @@ destination_table_id = "7x5qk53ddffctd3gtqmtvbujiq"
 destination_env = "migrate"
 
 
-# response = source_client.admin_get_user(
-#     UserPoolId='eu-central-1_rni4BQPaR',
-#     Username='Google_104475194586241570827'
-# )
+response = source_client.admin_get_user(
+    UserPoolId='eu-central-1_rni4BQPaR',
+    Username='Google_104475194586241570827'
+)
 
-# userGroups = source_client.admin_list_groups_for_user(
-#     UserPoolId='eu-central-1_rni4BQPaR',
-#     Username=response["Username"]
-# )
+userGroups = source_client.admin_list_groups_for_user(
+    UserPoolId='eu-central-1_rni4BQPaR',
+    Username=response["Username"]
+)
 
-# isGroupLeader = any([group["GroupName"] == "groupLeader" for group in userGroups["Groups"]])
-# if isGroupLeader:
-#     print("IsGroupLeader!")
-# isAdmin = any([group["GroupName"] == "groupLeader" for group in userGroups["Groups"]])
-# if isAdmin:
-#     print("IsAdmin!")
+isGroupLeader = any([group["GroupName"] == "groupLeader" for group in userGroups["Groups"]])
+if isGroupLeader:
+    print("IsGroupLeader!")
+isAdmin = any([group["GroupName"] == "groupLeader" for group in userGroups["Groups"]])
+if isAdmin:
+    print("IsAdmin!")
 
-# userName = response["Username"]
-# userAttributes = [item for item in response['UserAttributes'] if item['Name'] != 'sub' and item['Name'] != 'identities']
-# email = [item for item in response['UserAttributes'] if item['Name'] == 'email'][0]['Value']
-# userAttributes.append({"Name": "custom:OrganizationID", "Value": "knowitobjectnet"})
+userName = response["Username"]
+userAttributes = [item for item in response['UserAttributes'] if item['Name'] != 'sub' and item['Name'] != 'identities']
+email = [item for item in response['UserAttributes'] if item['Name'] == 'email'][0]['Value']
+userAttributes.append({"Name": "custom:OrganizationID", "Value": "knowitobjectnet"})
 
-# source_client.admin_create_user(
-#     UserPoolId='eu-central-1_r8n2aDwVQ',
-#     Username=email,
-#     MessageAction="SUPPRESS",
-#     UserAttributes=userAttributes
-# )
+source_client.admin_create_user(
+    UserPoolId='eu-central-1_r8n2aDwVQ',
+    Username=email,
+    MessageAction="SUPPRESS",
+    UserAttributes=userAttributes
+)
 
-# source_client.admin_add_user_to_group(
-#     UserPoolId='eu-central-1_r8n2aDwVQ',
-#     Username=email,
-#     GroupName="knowitobjectnet"
-# )
+source_client.admin_add_user_to_group(
+    UserPoolId='eu-central-1_r8n2aDwVQ',
+    Username=email,
+    GroupName="knowitobjectnet"
+)
 
-# if isGroupLeader:
-#     source_client.admin_add_user_to_group(
-#         UserPoolId='eu-central-1_r8n2aDwVQ',
-#         Username=email,
-#         GroupName="knowitobjectnet0groupLeader"
-#     )
+if isGroupLeader:
+    source_client.admin_add_user_to_group(
+        UserPoolId='eu-central-1_r8n2aDwVQ',
+        Username=email,
+        GroupName="knowitobjectnet0groupLeader"
+    )
     
-# if isAdmin:
-#     source_client.admin_add_user_to_group(
-#         UserPoolId='eu-central-1_r8n2aDwVQ',
-#         Username=email,
-#         GroupName="knowitobjectnet0admin"
-#     )
+if isAdmin:
+    source_client.admin_add_user_to_group(
+        UserPoolId='eu-central-1_r8n2aDwVQ',
+        Username=email,
+        GroupName="knowitobjectnet0admin"
+    )
 
 tables = [
-    # {
-    #     "name": "User",
-    #     "ownerAttribute": ["id"]
-    # },
+    {
+        "name": "User",
+        "ownerAttribute": ["id"]
+    },
     {
         "name": "UserForm",
         "ownerAttribute": ["owner"]
     },
-    # {
-    #     "name": "Category",
-    #     "ownerAttribute": ["orgAdmins", "organizationID"]
-    # },
-    # {
-    #     "name": "FormDefinition",
-    #     "ownerAttribute": ["orgAdmins", "organizationID"]
-    # },
-    # {
-        # "name": "Group",
-        # "ownerAttribute": ["groupLeaderUsername"]
-    # },
-    # {
-    #     "name": "Question",
-    #     "ownerAttribute": ["orgAdmins", "organizationID"]
-    # },
-    # {
-        # "name": "QuestionAnswer",
-        # "ownerAttribute": ["owner"]
-    # }
+    {
+        "name": "Category",
+        "ownerAttribute": ["orgAdmins", "organizationID"]
+    },
+    {
+        "name": "FormDefinition",
+        "ownerAttribute": ["orgAdmins", "organizationID"]
+    },
+    {
+        "name": "Group",
+        "ownerAttribute": ["groupLeaderUsername"]
+    },
+    {
+        "name": "Question",
+        "ownerAttribute": ["orgAdmins", "organizationID"]
+    },
+    {
+        "name": "QuestionAnswer",
+        "ownerAttribute": ["owner"]
+    }
 ]
 userName='Google_104475194586241570827'
 email='kristian.stamland@knowit.no'
 
 for table in tables:
     dynamopaginator = destination_dynamo_client.get_paginator('scan')
-    # source_tabname = table["name"] + '-' + destination_table_id + '-' + source_env
     destination_tabname = table["name"] + '-' + destination_table_id + '-' + destination_env
     dynamoresponse = dynamopaginator.paginate(
         TableName = destination_tabname,
@@ -106,9 +105,6 @@ for table in tables:
     )
     for page in dynamoresponse:
         for item in page['Items']:
-            # item['orgAdmins'] = 'knowitobjectnet0admin'
-            # item['orgGroupLeaders'] = 'knowitobjectnet0groupLeader'
-            # item['organizationID'] = 'knowitobjectnet'
             updateItem = False
             for tableItem in table["ownerAttribute"]:
                 if item[tableItem]["S"] == userName:
