6a68cc9d78ff0098ead3e30f4ae3da576f9e76ca
hakoncarlsen <hakon.carlsen@knowit.no>
2022-02-02 09:50:50 +0100

    changed add_api_key script to using argparse

--- scripts/add_api_key_hashed.py
+++ scripts/add_api_key_hashed.py
@@ -3,7 +3,18 @@ import json
 from hashlib import sha256
 import string
 import random
+import time
+import argparse
 
+parser = argparse.ArgumentParser(description='Add api_key to dynamodb')
+parser.add_argument('organization_ID', help='organizationID : This is a string which uniquely identifies the organization')
+parser.add_argument('api_key', help='API_Key: API_Key which can be used to access the api for the organization')
+
+
+args = parser.parse_args()
+
+organization_ID = args.organization_ID
+api_key = args.api_key
 
 with open('parameters.json') as parameters_file:
     parameters = json.load(parameters_file)
@@ -28,16 +39,14 @@ full_table_name = table_name + '-' + destination_graphql_api_id + '-' + destinat
 table = destination_dynamo_client.Table(full_table_name)
 
 
-organization_id = 'knowitsolutions'
-api_key = 'FIrEpJPxEG6UzHBYyXEsG5tIUENlw7yj1aWagLFH'
 api_key_hashed = sha256(api_key.encode('utf-8')).hexdigest()
 
-id = api_key_hashed + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)) + str(int(time.time()*1000))
 
 table.put_item(
     Item={
         "id": id,
         "APIKeyHashed": api_key_hashed,
-        "organizationID": organization_id
+        "organizationID": organization_ID
     }
 )
